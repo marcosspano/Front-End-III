24636b7d9fbc996547ea44e63fcf3c6fa1a90f91
marcosspano <e.marcosspano@gmail.com>
2021-11-17 00:13:22 -0300

    Atualizando

--- aula09_09112021_cpi/src/App.js
+++ aula09_09112021_cpi/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
-import './App.css';
+import './App.scss';
 
 export default class App extends Component {
 
