2b82a49914ba56d906fa496fb629d7b69059f795
marcosspano <e.marcosspano@gmail.com>
2021-11-22 11:56:30 -0300

    Atualizando

---
+++ aula14_22112021_mesadetrabalho/src/App.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './App.scss';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+
+      </>
+    );
+  }
+}
